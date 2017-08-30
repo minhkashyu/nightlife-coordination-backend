b51072826d12f7333e56c5cdc88d2189b42f1e4c
minhkashyu <minhvinhta@yahoo.com>
2017-08-30 14:23:32 +1000

    add BarSchema

---
+++ models/bar.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+const Schema = mongoose.Schema;
+
+const BarSchema = new Schema(
+    {
+        placeId: {
+            type: String,
+            required: true
+        },
+        userId: {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    },
+    {
+        timestamps: true
+    }
+);
+
+BarSchema.path('createdAt').expires('24h');
+
+export default mongoose.model('Bar', BarSchema);
