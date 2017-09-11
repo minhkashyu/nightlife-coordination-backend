cb2bf8bc2372535493dfd9fb44b2359cbfce722c
minhkashyu <minhvinhta@yahoo.com>
2017-09-11 14:00:27 +1000

    update error message

--- controllers/user/github.js
+++ controllers/user/github.js
@@ -4,12 +4,9 @@ import config from './../../config/main';
 
 export default {
     githubLogin: passport.authenticate('github', { scope : 'user:email', session: false }),
-    githubLoginCb: (req, res, next) => passport.authenticate('github', { session: false }, (err, user, info) => {
-        if (err) {
-            return next(err);
-        }
-        if (!user) {
-            res.status(400).json({ error: info.message });
+    githubLoginCb: (req, res, next) => passport.authenticate('github', { session: false }, (err, user) => {
+        if (err || !user) {
+            res.status(400).json({ error: 'Cannot connect to Github.' });
             return next();
         }
 
