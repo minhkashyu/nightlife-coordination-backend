2eee506e60fa98f006d461247a3c41eaa3ce1984
minhkashyu <minhvinhta@yahoo.com>
2017-09-12 21:11:45 +1000

    initial test

---
+++ tests/config/createdata.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+declare -a collections=("users" "bars")
+
+databaseName=$1
+
+if [ "$1" ]
+then
+    echo "$1"
+    for i in "${collections[@]}"
+    do
+        echo "Import $i"
+        mongoimport --db $1 --collection $i --drop --file ./tests/data/$i.json
+    done
+else
+    echo "Missing database name!"
+fi
