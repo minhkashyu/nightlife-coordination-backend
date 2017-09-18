bb2ac723ad6a7d0ffee15619e39151a7e5254991
minhkashyu <minhvinhta@yahoo.com>
2017-09-18 18:38:59 +1000

    add loginTest route for test

--- controllers/user/index.js
+++ controllers/user/index.js
@@ -1,10 +1,12 @@
 import requireAuth from './requireAuth';
 import github from './github';
 import loginSuccess from './loginSuccess';
+import loginTest from './loginTest';
 
 export default {
     requireAuth: requireAuth,
     githubLogin: github.githubLogin,
     githubLoginCb: github.githubLoginCb,
-    loginSuccess: loginSuccess
+    loginSuccess: loginSuccess,
+    loginTest: loginTest
 };
