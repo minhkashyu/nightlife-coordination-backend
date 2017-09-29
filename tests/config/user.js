cee83cc655e21157f4a344750ea0b578d7ff8332
minhkashyu <minhvinhta@yahoo.com>
2017-09-29 15:46:20 +1000

    add other user

--- tests/config/user.js
+++ tests/config/user.js
@@ -2,9 +2,19 @@ export default {
     githubUser: () => {
         return {
             id         : '59a96145597c4b02681ce47c',
+            userId     : '123456',
             username   : 'localtest',
             name       : 'Local Test',
             email      : 'localtest@hotmail.com'
+        };
+    },
+    secondGithubUser: () => {
+        return {
+            id         : '59a96145597c4b02681cf58d',
+            userId     : '789012',
+            username   : 'secondlocal',
+            name       : 'Second Local',
+            email      : 'secondlocal@hotmail.com'
         };
     }
 };
