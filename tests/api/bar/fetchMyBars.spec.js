16c11afa37c72d9a007922c1d48574cfb9fc28e1
minhkashyu <minhvinhta@yahoo.com>
2017-09-29 15:46:54 +1000

    fetchMyBars tests

---
+++ tests/api/bar/fetchMyBars.spec.js
@@ -0,0 +1,67 @@
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+chai.use(chaiHttp);
+let assert = chai.assert;
+import {describe, beforeEach, afterEach, it} from 'mocha';
+import _ from 'lodash';
+
+import auth from './../../config/auth';
+import user from './../../config/user';
+let app = require('./../../../server');
+let server;
+
+let newBar = {
+    placeId: 'ChIJO0bSoEx344kRMru3kL4jHzU',
+    name: 'Hops Test Kitchen & Raw Bar',
+    address: '1248 Cambridge St, Cambridge, MA 02139, United States'
+};
+
+describe('GET /api/bars', () => {
+    beforeEach(done => {
+        server = app.server;
+        done();
+    });
+
+    afterEach(done => {
+        server.close(done);
+    });
+
+    const callApi = () => chai.request(server).get('/api/bars');
+
+    it('it should NOT fetch my bars without authorization', done => {
+        callApi()
+            .end((err, res) => {
+                assert.equal(res.status, 401);
+                assert.equal(res.body.error, 'You are not authorised to do this. Please log in.');
+                done();
+            });
+    });
+
+    it('it should fetch my bars', done => {
+        auth.loginAsGithubUser(server)
+            .end((err, res) => {
+                callApi()
+                    .set('Authorization', res.body.token)
+                    .end((err, res) => {
+                        assert.equal(err, null);
+                        assert.equal(res.status, 200);
+
+                        let bars = res.body.bars;
+                        assert.equal(bars.length, 5);
+                        _.forEach(bars, bar => {
+                            assert.equal(bar.userId, user.githubUser().id);
+                        });
+
+                        assert.equal(res.body.goingBars.length, 1);
+                        let goingBar = res.body.goingBars[0];
+                        assert.equal(goingBar.placeId, newBar.placeId);
+                        assert.equal(goingBar.name, newBar.name);
+                        assert.equal(goingBar.address, newBar.address);
+                        assert.equal(goingBar.userId, user.githubUser().id);
+
+                        done();
+                    });
+            });
+
+    });
+});
