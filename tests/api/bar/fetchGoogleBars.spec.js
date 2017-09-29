d8ed928bd25464e80b9d895828e489207f9536fd
minhkashyu <minhvinhta@yahoo.com>
2017-09-29 13:54:37 +1000

    fetchGoogleBars tests

---
+++ tests/api/bar/fetchGoogleBars.spec.js
@@ -0,0 +1,90 @@
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+chai.use(chaiHttp);
+let assert = chai.assert;
+import {describe, beforeEach, afterEach, it} from 'mocha';
+import _ from 'lodash';
+
+import auth from './../../config/auth';
+import user from './../../config/user';
+let app = require('./../../../server');
+let server;
+
+let query = 'West End, Queensland, Australia';
+let min = 3;
+let newBar = {
+    placeId: 'ChIJO0bSoEx344kRMru3kL4jHzU',
+    name: 'Hops Test Kitchen & Raw Bar',
+    address: '1248 Cambridge St, Cambridge, MA 02139, United States'
+};
+
+describe('GET /api/places/:query', () => {
+    beforeEach(done => {
+        server = app.server;
+        done();
+    });
+
+    afterEach(done => {
+        server.close(done);
+    });
+
+    const callApi = (query) => chai.request(server).get(`/api/places/${query}`);
+
+    it('it should NOT fetch google bars without a search keyword', (done) => {
+        callApi('')
+            .end((err, res) => {
+                assert.equal(res.status, 400);
+                assert.equal(res.body.error, 'Please enter a search keyword.');
+                done();
+            });
+    });
+
+    it(`it should NOT fetch google bars if search keyword is less then ${min} characters`, (done) => {
+        callApi('te')
+            .end((err, res) => {
+                assert.equal(res.status, 400);
+                assert.equal(res.body.error, `Search Keyword must be ${min} characters or more`);
+                done();
+            });
+    });
+
+    it('it should fetch google bars without authorization', (done) => {
+        callApi(query)
+            .end((err, res) => {
+                assert.equal(err, null);
+                assert.equal(res.status, 200);
+                assert.ok(res.body.bars.length > 0);
+                assert.equal(res.body.goingBars.length, 0);
+                assert.equal(res.body.goingTotals.length, 1);
+                assert.equal(res.body.goingTotals[0]._id, newBar.placeId);
+                assert.equal(res.body.goingTotals[0].count, 1);
+                done();
+            });
+    });
+
+    it('it should fetch google bars with authorization', (done) => {
+        auth.loginAsGithubUser(server)
+            .end((err, res) => {
+                callApi(query)
+                    .set('Authorization', res.body.token)
+                    .end((err, res) => {
+                        assert.equal(err, null);
+                        assert.equal(res.status, 200);
+                        assert.ok(res.body.bars.length > 0);
+
+                        assert.equal(res.body.goingBars.length, 1);
+                        let goingBar = res.body.goingBars[0];
+                        assert.equal(goingBar.placeId, newBar.placeId);
+                        assert.equal(goingBar.name, newBar.name);
+                        assert.equal(goingBar.address, newBar.address);
+                        assert.equal(goingBar.userId, user.githubUser().id);
+
+                        assert.equal(res.body.goingTotals.length, 1);
+                        assert.equal(res.body.goingTotals[0]._id, newBar.placeId);
+                        assert.equal(res.body.goingTotals[0].count, 1);
+                        done();
+                    });
+            });
+
+    });
+});
